32d969b5dc8029f09e415ca7e97dc9010eb96b07
BraulioUI <uri-b11@outlook.com>
2020-08-22 13:58:10 -0500

    cambios en la interfaz de registro

--- Auriculoterapia/app/src/main/java/com/example/android/auriculoterapia_app/activities/RegisterActivity.kt
+++ Auriculoterapia/app/src/main/java/com/example/android/auriculoterapia_app/activities/RegisterActivity.kt
@@ -3,10 +3,17 @@ package com.example.android.auriculoterapia_app.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android.auriculoterapia_app.R
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //calendar
+
     }
 }
